0293501db6d821f391418dab631669ec62450e58
Maicolsc-msc <64378356+Maicolsc-msc@users.noreply.github.com>
2022-06-12 12:45:43 -0500

    Update instcheck.sh

--- Modulos/instcheck.sh
+++ Modulos/instcheck.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 clear
-echo "America/Sao_Paulo" > /etc/timezone
-ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
+echo -e "America/Lima" >/etc/localtime
+ln -fs /usr/share/zoneinfo/America/Lima /etc/localtime >/dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
-echo -e "\E[44;1;37m    INSTALAR CHECKUSER CONECTA4G     \E[0m" 
+echo -e "\E[44;1;37m    INSTALAR CHECKUSER CONECTA     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @ALFAINTERNET\033[1;36m"
+echo -e "                 \033[1;31mBy @MSCPERU\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
-echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
+echo -e "\n\033[1;36mINICIANDO INSTALACION \033[1;33mAGUARDE..."
 apt-get install figlet -y > /dev/null 2>&1
 pip3 install flask > /dev/null 2>&1
 rm /bin/initcheck > /dev/null 2>&1
@@ -36,6 +36,6 @@ clear
 echo ""
 echo -e "\033[1;31m \033[1;33mATIVER CHECKUSER NA OPÇÃO ( M ): \033[1;32m \033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@ALFAINTERNET\033[0m"
+echo -e "\033[1;33m MAS INFORMACION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@MSCPERU\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
